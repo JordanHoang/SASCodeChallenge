faa9b5ed1c21b048d249bd7a360231de2e33f37b
Jordan Hoang <jordan.n.hoang@gmail.com>
2020-02-19 12:54:28 -0600

    Set up index.html file and server file to serve up locally

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const parser = require('body-parser');
+
+const app = express()
+app.use(cors())
+app.use(parser())
+
+
+// ADD ADDITIONAL SERVER ROUTES ABOVE!!! WEBPACK CONFIGS //
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware')
+  const webpack = require('webpack')
+  const webpackConfig = require('../webpack.config.js')
+  app.use(webpackMiddleware(webpack(webpackConfig))) 
+} 
+
+app.use(express.static('dist'))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
+app.listen(process.env.PORT || 8080, () => console.log('Listening'))
